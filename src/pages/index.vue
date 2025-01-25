8fb8fcbfb7ee42f7ab2d1a7287080e3dda2ee1ef
babahdigital <babahdigital@gmail.com>
2025-01-25 16:35:28 +0800

    Auto-commit: 2025-01-25_16:35:28

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,25 +1,28 @@
 <template>
-  <div>
-    <VCard
-      class="mb-6"
-      title="Kick start your project 🚀"
-    >
-      <VCardText>All the best for your new project.</VCardText>
-      <VCardText>
-        Please make sure to read our <a
-          href="https://demos.pixinvent.com/vuexy-vuejs-admin-template/documentation/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="text-decoration-none"
-        >
-          Template Documentation
-        </a> to understand where to go from here and how to use our template.
-      </VCardText>
-    </VCard>
-
-    <VCard title="Want to integrate JWT? 🔒">
-      <VCardText>We carefully crafted JWT flow so you can implement JWT with ease and with minimum efforts.</VCardText>
-      <VCardText>Please read our  JWT Documentation to get more out of JWT authentication.</VCardText>
-    </VCard>
+  <div class="maintenance-container">
+    <h1>Under Maintenance</h1>
+    <p>We apologize for the inconvenience. Please check back later.</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'MaintenancePage',
+}
+</script>
+
+<style scoped>
+.maintenance-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #f0f2f5;
+  text-align: center;
+}
+h1 {
+  font-size: 3rem;
+  margin-bottom: 1rem;
+}
+</style>
